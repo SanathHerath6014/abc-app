63e9f43a460f64fda702e4f77b75918fc714aead
SanathHerath6014 <sanath6014nk@gmail.com>
2022-10-09 16:20:57 +0530

    ssecon commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import NaveBar from './components/NaveBar';
+import Home from './pages/Home';
+import News from './pages/News';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import BotomBar from './components/BotomBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NaveBar/>
+      <Home/>
+      <News/>
+      <Contact/>
+      <About/>
+      <BotomBar/>
+
     </div>
   );
 }
---
+++ src/components/BotomBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BotomBar() {
+  return (
+    <div>BotomBar</div>
+  )
+}
+
+export default BotomBar
